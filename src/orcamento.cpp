eb75511f31fc3c2376e965b1b8f4c4242a427124
juliamellolopes <juliamellolopesgoncalves@gmail.com>
2022-05-27 19:32:31 -0300

    subindo codigos dos orcamentos e verificacao do dia da semana

--- src/orcamento.cpp
+++ src/orcamento.cpp
@@ -2,22 +2,103 @@
 
 using namespace std;
 
+Orcamento()
+{
+    dia = mes = ano = porte_pequeno = porte_grande, formula, dia_semana = orcamento1 = orcamento2 = orcamento3 = 0;
+}
+
 // verificar se é ou não final de semana onde vai retornar 0 ou 1,
 // em que o 1 é para se for final de semana, senão retorna 0
-bool verificarDia(){}
+bool verificarDia()
+{
+    if (mes == 01) // mês de janeiro será o mês 13 do ano anterior.
+    {
+        mes = 13;
+        ano = ano - 1;
+    }
+
+    if (mes == 02) // mês de favereiro será o mês 14 do ano anterior.
+    {
+        mes = 14;
+        ano = ano - 1;
+    }
+
+    //calculo do dia da semana
+    formula = dia + 2 * mes + (3 * (mes + 1) / 5) + ano + ano / 4 - ano / 100 + ano / 400 + 2; 
+    dia_semana = formula % 7; 
+
+    if (mes == 13) // Se for mês de janeiro, coloca o valor certo do mês e do ano para mostrar na tela.
+    {
+        mes = 1;
+        ano = ano + 1;
+    }
+    if (mes == 14) // Se for mês de fevereiro, coloca o valor certo do mês e do ano para mostrar na tela.
+    {
+        mes = 2;
+        ano = ano + 1;
+    }
+
+    if (dia_semana == 0 && dia_semana == 1)
+        return 0 else return 1
+}
+
+void verfificarAno()
+{
+    // verifica se o ano é ou não Bissexto e se a data é valida
+    if ((ano % 4 == 0 && (ano % 400 == 0 || ano % 100 != 0)) && (dia > 0 && dia <= 31) && (mes > 0 && mes <= 12))
+    {
+        if (mes == 2 && dia > 29 || mes == 4 && dia > 30 || mes == 6 && dia > 30 || mes == 9 && dia > 30 || mes == 11 && dia > 30)
+        {
+            cout << "\nA data informada nao existe no calendario Gregoriano. \n";
+            exit;
+        }
+        else
+            verfificarDia();
+    }
+    else if (!(ano % 4 == 0 && (ano % 400 == 0 || ano % 100 != 0)) && (dia > 0 && dia <= 31) && (mes > 0 && mes <= 12))
+    {
+        if (mes == 2 && dia > 29 || mes == 4 && dia > 30 || mes == 6 && dia > 30 || mes == 9 && dia > 30 || mes == 11 && dia > 30)
+        {
+            cout << "\nA data informada nao existe no calendario Gregoriano. \n";
+            exit;
+        }
+        else
+            verfificarDia();
+    }
+    else
+        cout << "\nA data informada nao existe no calendario Gregoriano. \n";
+}
 
 // metodo de orçamento do MeuCaninoFeliz
-float orcamentoMeuCaninoFeliz() {}
+void orcamentoMeuCaninoFeliz()
+{
+    // calculo do orcamento em dias uteis
+    orcamento1 = (porte_pequeno * 20) + (porte_grande * 40);
+
+    if (verificarDia())
+        return orcamento;
+    else
+        return orcamento = +(orcamento * 0.20); // se for final de semana o orcamento tera um aumento de 20%
+}
 
 // metodo de orçamento do VaiRex
-float orcamentoVaiRex() {}
+void orcamentoVaiRex()
+{
+    if (verificarDia())
+        return orcamento2 = (porte_pequeno * 15) + (porte_grande * 50); // calculo do orcamento em dias uteis
+    else
+        return orcamento2 = (porte_pequeno * 20) + (porte_grande * 55); // calculo do orcamento nos finais de semana
+}
 
 // metodo de orçamento ChowChawgas
-float orcamentoChowChawgas() {}
+void orcamentoChowChawgas()
+{
+    orcamento3 = (porte_pequeno * 30) + (porte_grande * 45); // calculo do orcamento
+}
 
-// metodo de imprimir resultado
- void imprimirResultado(){}
-
-// comparando orcamentos, é o metodo principal, dele sera chamado o metodo de verificar dia, a apartir do dia vamos 
+// comparando orcamentos, é o metodo principal, dele sera chamado o metodo de verificar dia, a apartir do dia vamos
 // para a chamada os metodos de  orcamentos de cada local. Depois de comparar as respostas sera impresso o resultado
-void comparandoOrcamentos(){}
+void comparandoOrcamentos()
+{
+    
+}
--- src/orcamento.hpp
+++ src/orcamento.hpp
@@ -10,21 +10,23 @@ using namespace std;
 
 class Orcamento
 {
-    private:
-
-    public:
-        // verificar se é ou não final de semana
-            bool verificarDia();
-        // orçamento do MeuCaninoFeliz
-            float orcamentoMeuCaninoFeliz();
-        // orçamento do VaiRex
-            float orcamentoVaiRex();
-        // orçamento ChowChawgas
-            float orcamentoChowChawgas();
-        // imprimindo resultado
-            void imprimirResultado();
-        // comparando orcamentos
-            void comparandoOrcamentos();
+private:
+    int dia, mes, ano, porte_pequeno, porte_grande, formula, dia_semana;
+    float orcamento1, orcamento2, orcamento3;
+public:
+    Orcamento();
+    // verificar se é ou não final de semana
+    bool verificarDia();
+    // verficar se o ano é bisexto
+    void verfificarAno();
+    // orçamento do MeuCaninoFeliz
+    void orcamentoMeuCaninoFeliz();
+    // orçamento do VaiRex
+    void orcamentoVaiRex();
+    // orçamento ChowChawgas
+    void orcamentoChowChawgas();
+    // comparando orcamentos
+    void comparandoOrcamentos();
 };
 
 #endif
